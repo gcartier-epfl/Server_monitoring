d129a66cc6ade68c8801d2e024f9699e9c4c4d05
gcartier-epfl <gael.cartier-michaud@epfl.ch>
2024-01-23 14:34:56 +0100

    .

--- collector/collector_data.py
+++ collector/collector_data.py
@@ -15,7 +15,7 @@ def main() -> None :
     print( os.path.join( CWD, OUTPUT_FILE ) )
     timestamp = datetime.now().strftime( "%d/%m/%Y %H:%M:%S" )
     platform_info = platform.uname()
-    dump_json_to_file( { 'hostname' : platform_info.node, 'timestamp' : timestamp, 'system' : { 'os': platform_info.system, 'os_distri': platform_info.version, 'kernel': platform_info.release }}, os.path.join( CWD, OUTPUT_FILE ) )
+    dump_json_to_file( { 'hostname' : platform_info.node, 'timestamp' : timestamp, 'system' : { 'os': platform_info.system, 'os_distri': platform_info.version, 'kernel': platform_info.release }, 'path' : os.path.join( CWD, OUTPUT_FILE ) }, os.path.join( CWD, OUTPUT_FILE ) )
 
 
 main()

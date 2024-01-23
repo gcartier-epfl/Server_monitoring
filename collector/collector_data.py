a7836a17728eec92bbd127f6dfa1f9f77ce6dfe1
gcartier-epfl <gael.cartier-michaud@epfl.ch>
2024-01-23 14:39:53 +0100

    removed print causing mail sending

--- collector/collector_data.py
+++ collector/collector_data.py
@@ -12,7 +12,6 @@ def dump_json_to_file( dict, filename ) -> None :
 def main() -> None : 
     CWD = os.getcwd()
     OUTPUT_FILE = "collected_data.json"
-    print( os.path.join( CWD, OUTPUT_FILE ) )
     timestamp = datetime.now().strftime( "%d/%m/%Y %H:%M:%S" )
     platform_info = platform.uname()
     dump_json_to_file( { 'hostname' : platform_info.node, 'timestamp' : timestamp, 'system' : { 'os': platform_info.system, 'os_distri': platform_info.version, 'kernel': platform_info.release }, 'path' : os.path.join( CWD, OUTPUT_FILE ) }, os.path.join( CWD, OUTPUT_FILE ) )

f547839f1215e8fac2bfe3a9da048862c7365f42
gcartier-epfl <gael.cartier-michaud@epfl.ch>
2024-01-29 16:18:05 +0100

    install scipt from python to bash

---
+++ install/install_collector.sh
@@ -0,0 +1,17 @@
+#!/bin/bash  
+
+USER="gcartier" 
+GROUP="gcartier" 
+WORKSPACE="/var/users/gcartier/" 
+COLLECTOR_PATH="/var/users/gcartier/collector/"
+VENV_NAME=".env"
+
+if [ ! -d $(COLLECTOR_PATH) ]; then
+    sudo mkdir -p $(COLLECTOR_PATH);
+    sudo chown $(USER):$(GROUP) -r $(WORKSPACE)
+fi
+
+cp ../collector/*.py $(COLLECTOR_PATH)
+python3 -m pip install virtualenv 
+virtualenv $(WORKSPACE)/$(VENV_NAME) 
+source $(WORKSPACE)/$(VENV_NAME)/bin/activate

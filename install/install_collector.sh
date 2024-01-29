b162519adf2a2eb82b22fa4deea9f855262052f4
gcartier-epfl <gael.cartier-michaud@epfl.ch>
2024-01-29 16:23:08 +0100

    script from python to bash

--- install/install_collector.sh
+++ install/install_collector.sh
@@ -6,12 +6,12 @@ WORKSPACE="/var/users/gcartier/"
 COLLECTOR_PATH="/var/users/gcartier/collector/"
 VENV_NAME=".env"
 
-if [ ! -d $(COLLECTOR_PATH) ]; then
-    sudo mkdir -p $(COLLECTOR_PATH);
-    sudo chown $(USER):$(GROUP) -r $(WORKSPACE)
+if [ ! -d "$(COLLECTOR_PATH)" ]; then
+    sudo mkdir -p "$(COLLECTOR_PATH)";
+    sudo chown "$(USER)":"$(GROUP)" -r "$(WORKSPACE)"
 fi
 
-cp ../collector/*.py $(COLLECTOR_PATH)
+cp ../collector/*.py "$(COLLECTOR_PATH)"
 python3 -m pip install virtualenv 
-virtualenv $(WORKSPACE)/$(VENV_NAME) 
-source $(WORKSPACE)/$(VENV_NAME)/bin/activate
+virtualenv "$(WORKSPACE)"/"$(VENV_NAME)" 
+source "$(WORKSPACE)"/"$(VENV_NAME)"/bin/activate

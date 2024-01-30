e585fae982e0200466b8e14d59564bbb9a4ecc15
gcartier-epfl <gael.cartier-michaud@epfl.ch>
2024-01-30 10:56:28 +0100

    installation script: Crontjob installation witch check

--- test.sh
+++ test.sh
@@ -1,2 +1,14 @@
-DISTRI=$(. /etc/os-release; echo $ID)
-echo $DISTRI
+#!/bin/bash
+
+# DISTRI=$(. /etc/os-release; echo $ID)
+# echo $DISTRI
+
+CRON_JOBS="$(crontab -l)"
+CRONJOB="SHELL=/bin/bash\n*/5     *       *       *	*    	cd /var/users/gcartier/Server_monitoring/collector/ && python3 /var/users/gcartier/Server_monitoring/collector/collector_data.py"
+
+if [ "$CRON_JOBS" = "$(echo -e $CRONJOB)" ]; then
+    echo ">>> The collector cronjob is already installed";
+else
+    echo ">>> Cronjob installation"
+    # (sudo crontab -u $USER -l; echo -e "$CRONJOB" ) | sudo crontab -u $USER - 
+fi

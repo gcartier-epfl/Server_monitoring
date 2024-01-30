41273a96cc50218ff4eed7766df649be2fa8670f
gcartier-epfl <gael.cartier-michaud@epfl.ch>
2024-01-30 09:05:59 +0100

    python package install and cronjob adding

--- install/install_collector.sh
+++ install/install_collector.sh
@@ -15,4 +15,11 @@ fi
 cp collector/*.py $COLLECTOR_PATH
 python3 -m pip install virtualenv 
 virtualenv $WORKSPACE/$VENV_NAME 
-source $WORKSPACE/$VENV_NAME/bin/activate
+source $WORKSPACE/$VENV_NAME/bin/activate #FONCTIONNE POUR LE TEMPS D'EXECUTION DU SCRIPT
+
+# Installation des paquets python  
+python3 -m pip install -r requirements.txt
+
+# Ajout du cronjob au crontab  
+CRONJOB="SHELL=/bin/bash\n*/5     *       *       *	*    	cd /var/users/gcartier/Server_monitoring/collector/ && python3 /var/users/gcartier/Server_monitoring/collector/collector_data.py"
+(crontab -u $USER -l; echo -e "$CRONJOB" ) | crontab -u $USER - 
